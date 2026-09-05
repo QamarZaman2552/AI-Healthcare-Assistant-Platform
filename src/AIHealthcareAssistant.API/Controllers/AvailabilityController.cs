66c391d5e3727bcfa544fda476ab0162467b9cde
Ahsan Fayyaz <ahsanfayyaz955@gmail.com>
2026-09-05 18:06:41 +0500

    Add Availability Controller Endpoints

---
+++ src/AIHealthcareAssistant.API/Controllers/AvailabilityController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AIHealthcareAssistant.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class AvailabilityController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Create()
+        {
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok();
+        }
+    }
+}
