73f311fab5c7e62bdb223a2732d24ca2fe9c64ce
Ahsan Fayyaz <ahsanfayyaz955@gmail.com>
2026-09-07 14:25:31 +0500

    Add admin appointment and AI controllers

---
+++ src/AIHealthcareAssistant.API/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AIHealthcareAssistant.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize(Roles = "Admin")]
+    public class AdminController : ControllerBase
+    {
+        [HttpGet("dashboard")]
+        public IActionResult GetDashboard()
+        {
+            return Ok();
+           
+        }
+
+        [HttpGet("users")]
+        public IActionResult GetUsers()
+        {
+            return Ok();
+          
+        }
+
+        [HttpGet("system-status")]
+        public IActionResult GetSystemStatus()
+        {
+            return Ok();
+           
+        }
+    }
+}
