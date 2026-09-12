7431325a8d890c11473450e7f81e7d4cb5acf99d
QAMAR ZAMAN BALOCH <qamarbaloch2023@gmail.com>
2026-09-12 13:51:17 +0500

    feat: add auto migration on startup

--- src/AIHealthcareAssistant.API/Program.cs
+++ src/AIHealthcareAssistant.API/Program.cs
@@ -1,5 +1,7 @@
 using AIHealthcareAssistant.API;
 using AIHealthcareAssistant.Infrastructure;
+using AIHealthcareAssistant.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +9,12 @@ builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApi();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 if (app.Environment.IsDevelopment())
 {
