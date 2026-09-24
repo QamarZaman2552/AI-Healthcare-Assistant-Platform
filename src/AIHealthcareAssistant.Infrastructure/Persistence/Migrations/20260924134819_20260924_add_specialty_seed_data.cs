0c34916fb9a6097707eec00389a53f1bdc514a9e
QAMAR ZAMAN BALOCH <qamarbaloch2023@gmail.com>
2026-09-24 18:50:07 +0500

    feat: add specialty seed data migration

---
+++ src/AIHealthcareAssistant.Infrastructure/Persistence/Migrations/20260924134819_20260924_add_specialty_seed_data.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AIHealthcareAssistant.Infrastructure.src.AIHealthcareAssistant.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class _20260924_add_specialty_seed_data : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
